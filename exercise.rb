8f9ca390478a4f64a48e88f3fe0b00870e1b92db
Marlon O'Neil <hello@marlononeil.com>
2017-08-11 23:40:52 -0400

    completed step 1

---
+++ exercise.rb
@@ -0,0 +1,20 @@
+trains = [
+{train: "72C", frequency_in_minutes: 15, direction: "north"},
+{train: "72D", frequency_in_minutes: 15, direction: "south"},
+{train: "610", frequency_in_minutes: 5, direction: "north"},
+{train: "611", frequency_in_minutes: 5, direction: "south"},
+{train: "80A", frequency_in_minutes: 30, direction: "east"},
+{train: "80B", frequency_in_minutes: 30, direction: "west"},
+{train: "110", frequency_in_minutes: 15, direction: "north"},
+{train: "111", frequency_in_minutes: 15, direction: "south"}
+]
+
+# Save the direction of train 111 into a variable.
+train111_direction = ""
+
+trains.each do |train|
+  if train[:train] == "111"
+    train111_direction = train[:direction]
+  end
+end
+puts "Train 111 is heading #{train111_direction}"
