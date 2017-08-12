de31ac64f130daa1207052572211c42d1f811229
Marlon O'Neil <hello@marlononeil.com>
2017-08-11 23:43:05 -0400

    completed all steps, tried commiting per step, didn't bother

--- exercise.rb
+++ exercise.rb
@@ -27,3 +27,59 @@ trains.each do |train|
   end
 end
 puts "The frequency of train 80B is #{train80B_frequency} minutes"
+
+
+# Save the direction of train 610 into a variable.
+train610_direction = ""
+
+trains.each do |train|
+  if train[:train] == "610"
+    train610_direction = train[:direction]
+  end
+end
+puts "Train 610 is heading #{train610_direction}"
+
+# Create an empty array. Iterate through each train and add the name of the train into the array if it travels north.
+northern_trains = []
+trains.each do |train|
+  if train[:direction] == "north"
+    northern_trains << train[:train]
+  end
+end
+puts "Trains #{northern_trains.join(", ")} are heading north"
+
+# Do the same thing for trains that travel east.
+eastern_trains = []
+trains.each do |train|
+  if train[:direction] == "east"
+    eastern_trains << train[:train]
+  end
+end
+puts "Train #{eastern_trains.join(", ")} is heading east"
+
+# You probably just ended up rewriting some of the same code. Move this repeated code into a method that accepts a direction and a list of trains as arguments, and returns a list of just the trains that go in that direction. Call this method once for north and once for east in order to DRY up your code.
+def get_list_of_trains_by_direction(trains, direction)
+
+  trains.each do |train|
+    if train[:direction] == direction
+      puts "Train #{train[:train]} is heading #{direction}"
+    end
+  end
+
+end
+
+puts
+puts "These trains go north"
+get_list_of_trains_by_direction(trains, "north")
+puts
+puts "These trains go east"
+get_list_of_trains_by_direction(trains, "east")
+
+
+# Pick one train and add another key/value pair for the first_departure_time. For simplicity, assume the first train always leave on the hour. You can represent this hour as an integer: 6 for 6:00am, 12 for noon, 13 for 1:00pm, etc.
+trains.each do |train|
+  if train[:train] == "80B"
+    train[:first_departure_time] = 6
+  end
+end
+puts trains
